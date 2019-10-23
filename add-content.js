54fdf58f2f3e4dd3b876d6b964f21426b9074809
Xandria Bramble <xabpinkninja13@gmail.com>
2019-10-23 18:12:25 -0400

    input javascript conditions & questions

---
+++ add-content.js
@@ -0,0 +1,22 @@
+
+var age = prompt('How old are you?');
+
+if (age <= 18) {
+    alert('You still in school ah?');
+} else if (age > 18) {
+    alert('You went to college ah?')
+}
+
+var music = prompt('How many instruments do you play?');
+
+if (music > 2) {
+    alert('You are multi-instrumental, like me.')
+    }  else if (music <= 2) {
+    alert('You\'re a mediocore musician.')
+}      
+                    -
+document.write('<h3>' + greeting + '</h3>');
+
+
+
+
